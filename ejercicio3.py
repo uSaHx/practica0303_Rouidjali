1f7cd7a78ccb0de55044b54d2561bb224b7b19ba
uSaHx <orouidj@educacion.navarra.es>
2023-02-06 11:40:49 +0100

    06

---
+++ ejercicio3.py
@@ -0,0 +1,49 @@
+
+import pygame
+
+pygame.init()
+ventana = pygame.display.set_mode((640,480))
+pygame.display.set_caption("Ejemplo 3")
+
+ball = pygame.image.load("ball.png")
+ballrect = ball.get_rect()
+speed = [4,4]
+ballrect.move_ip(0,0)
+
+# Crea el objeto bate, y obtengo su rectángulo
+bate = pygame.image.load("barra.png")
+baterect = bate.get_rect()
+
+# Pongo el bate en la parte inferior de la pantalla
+baterect.move_ip(240,450)
+
+jugando = True
+while jugando:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            jugando = False
+
+    # Compruebo si se ha pulsado alguna tecla
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        baterect = baterect.move(-3,0)
+    if keys[pygame.K_RIGHT]:
+        baterect = baterect.move(3,0)
+
+    # Compruebo si hay colisión
+    if baterect.colliderect(ballrect):
+        speed[1] = -speed[1]
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > ventana.get_width():
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
+        speed[1] = -speed[1]
+    ventana.fill((252, 243, 207))
+    ventana.blit(ball, ballrect)
+
+    # Dibujo el bate
+    ventana.blit(bate, baterect)
+    pygame.display.flip()
+    pygame.time.Clock().tick(60)
+
+pygame.quit()
